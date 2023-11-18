b39e8d952425d4f22cd81514cea065006150f41f
buchserge <70943936+buchserge@users.noreply.github.com>
2023-11-18 18:30:25 +0300

    refactor sql script

--- hw3/src/main/webapp/resources/database_def.sql
+++ hw3/src/main/webapp/resources/database_def.sql
@@ -1,3 +1,9 @@
+CREATE TABLE professor (
+  id bigint NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE course (
   id_course bigint NOT NULL AUTO_INCREMENT,
   title varchar(255) NOT NULL,
@@ -5,6 +11,12 @@ CREATE TABLE course (
   PRIMARY KEY (id_course),
   KEY fk_prof (professor_id),
   CONSTRAINT fk_prof FOREIGN KEY (professor_id) REFERENCES professor (id)
+);
+
+CREATE TABLE student (
+  id bigint NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE course_student (
@@ -15,15 +27,3 @@ CREATE TABLE course_student (
   CONSTRAINT course_student_fk_1 FOREIGN KEY (course_id) REFERENCES course (id_course),
   CONSTRAINT course_student_fk_2 FOREIGN KEY (student_id) REFERENCES student (id)
 );
-
-CREATE TABLE professor (
-  id bigint NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE student (
-  id bigint NOT NULL AUTO_INCREMENT,
-  name varchar(255) NOT NULL,
-  PRIMARY KEY (id)
-)
